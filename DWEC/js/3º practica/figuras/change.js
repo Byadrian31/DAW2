2e674a8b61708976c8bbe15f93054338a8834998
Adrian <adrianlp922@gmail.com>
2024-09-26 15:59:23 +0200

    Pequeñas modificaciones

---
+++ DWEC/js/3º practica/figuras/change.js
@@ -0,0 +1,9 @@
+function changeCircle() {
+    //document.getElementById("red").style.borderRadius("50%");
+    document.getElementsByClassName("cuadrado").style.borderRadius("50%");
+}
+
+function changeSquare() {
+    //document.getElementById("red").style.borderRadius("50%");
+    document.getElementsByClassName("cuadrado").style.borderRadius("50%");
+}
