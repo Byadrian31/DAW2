d37a89ee2d0314175639f78dd901dadd88272fbd
Adrian <adrianlp922@gmail.com>
2024-10-01 16:18:19 +0200

    actualizacion figuras(INCOMPLETO)

--- DWEC/js/3º practica/figuras/change.js
+++ DWEC/js/3º practica/figuras/change.js
@@ -1,9 +1,15 @@
-function changeCircle() {
-    //document.getElementById("red").style.borderRadius("50%");
-    document.getElementsByClassName("cuadrado").style.borderRadius("50%");
+function changeCircle(element) {
+    element.style.transition = "1s"
+    element.style.borderRadius = "50%"
+    //document.getElementsByClassName("cuadrado").style.borderRadius("50%");
 }
 
-function changeSquare() {
+function changeSquare(element) {
     //document.getElementById("red").style.borderRadius("50%");
-    document.getElementsByClassName("cuadrado").style.borderRadius("50%");
+    element.style.transition = "1s"
+    element.style.borderRadius = "0%"
+}
+
+function shadowOut(element) {
+    element.style.shadow = "0px 0px 0px"
 }
